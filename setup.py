0559e2e54629c8058c7d5aec4c232d8731822d0b
willbelr <william.belr@gmail.com>
2018-01-29 15:48:19 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import os
+import setuptools
+import setuptools.command.build_py
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class CreateDesktopFile(setuptools.command.build_py.build_py):
+  def run(self):
+    path = here + "/qtpad.desktop"
+    with open(os.path.join(path), 'w') as f:
+        f.write("[Desktop Entry]\n")
+        f.write("Name=qtpad\n")
+        f.write("Exec=qtpad\n")
+        f.write("Terminal=false\n")
+        f.write("Type=Application\n")
+        f.write("Icon=view-compact-symbolic")
+    setuptools.command.build_py.build_py.run(self)
+
+
+# Workaround in case PyQt5 was installed without pip
+install_requires=['requests']
+try:
+    import PyQt5
+except ImportError:
+    install_requires.append("pyqt5")
+
+setuptools.setup(
+    name='qtpad',
+    version='1.0.0',
+    description='Modern and customizable sticky note application',
+    url='https://github.com/willbelr/qtpad',
+    keywords='sticky note text editor note-taking',
+
+    # From https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    cmdclass={'build_py': CreateDesktopFile},
+    data_files=[('share/applications/', ['qtpad.desktop'])],
+    include_package_data=True,
+    package_data={'': ['*.ui', 'icons/*.svg']},
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'qtpad=qtpad:main',
+        ],
+    },
+)
