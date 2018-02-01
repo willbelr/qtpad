a1bd4ab449e12c7b45e0cc04835e503960debc0a
willbelr <william.belr@gmail.com>
2018-02-01 14:00:50 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ class CreateDesktopFile(setuptools.command.build_py.build_py):
     with open(os.path.join(here + "/qtpad.desktop"), 'w') as f:
         f.write("[Desktop Entry]\n")
         f.write("Name=qtpad\n")
-        f.write("Exec=qtpad\n")
+        f.write("GenericName=Sticky Notes\n")
         f.write("Terminal=false\n")
         f.write("Type=Application\n")
-        f.write("Icon=view-compact-symbolic")
+        f.write("Categories=Utility;\n")
+        f.write("Icon=qtpad\n")
+        f.write("Exec=qtpad\n")
     setuptools.command.build_py.build_py.run(self)
 
 
@@ -27,7 +29,7 @@ except:
 
 setuptools.setup(
     name='qtpad',
-    version='0.0.2',
+    version='0.0.3',
     description='Modern and customizable sticky note application',
     keywords='sticky note text editor note-taking',
     author='William Belanger',
@@ -43,7 +45,10 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
     ],
     cmdclass={'build_py': CreateDesktopFile},
-    data_files=[('share/applications/', ['qtpad.desktop'])],
+    data_files=[
+        ('share/icons/hicolor/scalable/apps', ['qtpad.svg']),
+        ('share/applications', ['qtpad.desktop'])
+    ],
     package_data={'': ['icons/*.svg']},
     packages=setuptools.find_packages(),
     install_requires=install_requires,
